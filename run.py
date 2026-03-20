69ce28b05f13aa495548b862b75d50252f3ac093
Priyanshu Priya <priyanshupriyacodes@gmail.com>
2026-03-20 21:54:33 +0530

    dummy server created to host in render web services

--- run.py
+++ run.py
@@ -3,7 +3,28 @@ Resonance Bot — Telegram content curation assistant.
 Run this file to start the bot.
 """
 
+# from bot.main import main
+
+# if __name__ == "__main__":
+#     main()
+
+# Alternate fix for Render WebService
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 from bot.main import main
 
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
 if __name__ == "__main__":
+    threading.Thread(target=run_web).start()
     main()
